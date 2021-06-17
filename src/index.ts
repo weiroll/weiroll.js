f18b237be8e6d3c45ad4923aeb397c786f03711b
Jeff Reiner <theundeadghost@gmail.com>
2021-06-17 15:23:54 +0200

    Remove exports thay don't exist

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 'use strict';
 
-export {
-  isReturnValue,
-  Contract,
-  ContractFunction,
-  FunctionCall,
-  Value,
-  StaticValue,
-  ReturnValue,
-} from './contract';
-
 export { Planner } from './planner';
