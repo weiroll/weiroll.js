906a30b5757dd7c3bae19ba13369d44302aefd13
Jeff Reiner <theundeadghost@gmail.com>
2021-06-18 13:50:49 +0200

    Revert "Remove exports thay don't exist"
    This reverts commit f18b237be8e6d3c45ad4923aeb397c786f03711b.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 'use strict';
 
+export {
+  isReturnValue,
+  Contract,
+  ContractFunction,
+  FunctionCall,
+  Value,
+  StaticValue,
+  ReturnValue,
+} from './contract';
+
 export { Planner } from './planner';
